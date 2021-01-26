2d4c75dfeeda5e33954656fb6dc216f319ad5b94
Tom Stellard <tstellar@redhat.com>
2021-01-26 23:52:03 +0000

    Add get-llvm-version action

---
+++ get-llvm-version/index.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+const exec = require('child_process');
+const srcdir = core.getInput('srcdir');
+const cmd = "grep -o 'LLVM_VERSION_\(MAJOR\|MINOR\|PATCH\) [0-9]\+'" + srcdir + "/llvm/CMakeLists.txt"
+
+exec(cmd, (error, stdout, stderr) => {
+  stdout.split("\n").forEach(function(line) {
+    data = line.split(" ");
+    core.setOutput(data[0], data[1]);
+  });
+}
