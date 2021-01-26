94577ddd7890c474f5304e8e9a58202c24c7a37b
Tom Stellard <tstellar@redhat.com>
2021-01-26 23:52:03 +0000

    Add get-llvm-version action

---
+++ get-llvm-version/README.md
@@ -0,0 +1,6 @@
+# How to update
+
+* Make changes to index.js
+* Run: npm install
+* Run: npm run-script build
+* Commit changes to index.js and dist/index.js
---
+++ get-llvm-version/index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const { exec } = require('child_process');
+const srcdir = core.getInput('srcdir');
+const cmd = "grep -o 'LLVM_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) [0-9]\\+' " + srcdir + "/llvm/CMakeLists.txt"
+
+console.log(cmd);
+exec(cmd, (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  stdout.split("\n").forEach(function(line) {
+    data = line.split(" ");
+    if (data[0])
+      core.setOutput(data[0], data[1]);
+  });
+});
