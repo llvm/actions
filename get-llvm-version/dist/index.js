9c839a9d2febaef0d339c080027faafcb5a92885
Tom Stellard <tstellar@redhat.com>
2021-01-27 00:40:11 +0000

    Update

--- get-llvm-version/dist/index.js
+++ get-llvm-version/dist/index.js
@@ -10,11 +10,18 @@ const { exec } = __nccwpck_require__(129);
 const srcdir = core.getInput('srcdir');
 const cmd = "grep -o 'LLVM_VERSION_\(MAJOR\|MINOR\|PATCH\) [0-9]\+'" + srcdir + "/llvm/CMakeLists.txt"
 
+console.log(cmd);
 exec(cmd, (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
   stdout.split("\n").forEach(function(line) {
     data = line.split(" ");
     core.setOutput(data[0], data[1]);
-  })
+  });
 });
 
 
--- get-llvm-version/index.js
+++ get-llvm-version/index.js
@@ -3,9 +3,16 @@ const { exec } = require('child_process');
 const srcdir = core.getInput('srcdir');
 const cmd = "grep -o 'LLVM_VERSION_\(MAJOR\|MINOR\|PATCH\) [0-9]\+'" + srcdir + "/llvm/CMakeLists.txt"
 
+console.log(cmd);
 exec(cmd, (error, stdout, stderr) => {
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
   stdout.split("\n").forEach(function(line) {
     data = line.split(" ");
     core.setOutput(data[0], data[1]);
-  })
+  });
 });
